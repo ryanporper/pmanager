9cfd89a2b32f0cfa1469b84183326488bd5e93e4
ryanporper <ryan_porper@aol.com>
2022-09-13 18:17:37 -0400

    product manager

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const port = 8000;
 
-const { destinationRouter } = require('./routes/destination.routes');
+const { productRouter } = require('./routes/product.routes');
 
 require('./config/mongoose.config');
 
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/api/destinations', destinationRouter);
+app.use('/api/products', productRouter);
 
 app.listen(port, () =>
   console.log(`Listening on port ${port} for REQuests to RESpond to.`)
